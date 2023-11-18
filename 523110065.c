417754ae1db1e297827030e9a419dd3f5b5a03f0
Devendra <sharmadev7004@gmail.com>
2023-11-18 11:02:06 +0530

    Devendra Suthiya Rollno.-523110065

--- 523110065.c
+++ 523110065.c
@@ -1,25 +1,27 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-    int a[5];
-    int temp,*ptr;
-    ptr=&temp;
+  
+    int temp,n,*a;
     printf("Devendra Suthiya , 523110065");
-    printf("Enter 5 elements :");
-    for(int i=0;i<5;i++)
+    printf("\nEnter capacity of array  :");
+    scanf("%d",&n);
+    a=(int *)malloc(sizeof(int)*n);
+    for(int i=0;i<n;i++)
     {
         printf("\nEnter %d index element : ",i);
         scanf("%d",a+i);
         
     }
     printf("\narray intially :");
-    for(int i=0;i<5;i++)
+    for(int i=0;i<n;i++)
     printf("%d ",a[i]);
-    for(int i=0;i<5/2;i++)
+    for(int i=0;i<n/2;i++)
     {
-        *ptr=a[i];
-        a[i]=a[4-i];
-        a[4-i]=*ptr;
+        temp=a[i];
+        a[i]=a[n-1-i];
+        a[n-1-i]=temp;
     }
     printf("\nArray after reverse : ");
     for(int i=0;i<5;i++)
